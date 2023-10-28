90b4cf44f983f0ae71346b39ea1dd62561f25c75
Jesper van den Ende <jespertheend@gmail.com>
2023-10-29 01:47:45 +0200

    refactor: Use TypedMessenger for communication

--- src/WebSocketConnection.js
+++ src/WebSocketConnection.js
@@ -1,21 +1,26 @@
-import { generateUuid } from "https://raw.githubusercontent.com/rendajs/Renda/7adb3535329160690dbc6c136eca6327e9082c73/src/util/util.js";
+import { generateUuid } from "https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/util/util.js";
+import { TypedMessenger } from "https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/util/TypedMessenger.js";
+
+/**
+ * @typedef {ReturnType<WebSocketConnection["getResponseHandlers"]>} StudioDescoveryResponseHandlers
+ */
 
 export class WebSocketConnection {
 	#webSocketManager;
 	#remoteAddress;
-	#id;
-	get id() {
-		return this.#id;
+	#uuid;
+	get uuid() {
+		return this.#uuid;
 	}
-	#rawConnection;
 
-	get clientType() {
-		return "studio"; // todo: support for setting this from client
-	}
-	#projectMetaData = null;
+	/** @type {TypedMessenger<StudioDescoveryResponseHandlers, import("https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/network/studioConnections/discoveryMethods/WebRtcDiscoveryMethod.js").ExternalDiscoveryManagerResponseHandlers>} */
+	#messenger;
 
-	#firstIsHostMessageReceived = false;
-	#isStudioHost = false;
+	/** @type {import("https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/network/studioConnections/DiscoveryManager.js").ClientType?} */
+	#clientType = null;
+
+	/** @type {import("https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/network/studioConnections/DiscoveryManager.js").RemoteStudioMetadata?} */
+	#projectMetadata = null;
 
 	/**
 	 * @param {import("./WebSocketManager.js").WebSocketManager} webSocketManager
@@ -25,147 +30,124 @@ export class WebSocketConnection {
 	constructor(webSocketManager, remoteAddress, rawConnection) {
 		this.#webSocketManager = webSocketManager;
 		this.#remoteAddress = remoteAddress;
-		this.#id = generateUuid();
-		this.#rawConnection = rawConnection;
+		this.#uuid = generateUuid();
 
-		this.#rawConnection.addEventListener("message", this.onMessage.bind(this));
+		this.#messenger = new TypedMessenger();
+		this.#messenger.initializeWebSocket(rawConnection, this.getResponseHandlers());
+		this.#messenger.configureSendOptions({
+			addAvailableConnection: {
+				expectResponse: false,
+			},
+			removeAvailableConnection: {
+				expectResponse: false,
+			},
+			setAvailableConnections: {
+				expectResponse: false,
+			},
+			setConnectionProjectMetadata: {
+				expectResponse: false,
+			},
+			relayMessage: {
+				expectResponse: false,
+			},
+		});
 	}
 
-	/**
-	 * @param {MessageEvent} message
-	 */
-	onMessage(message) {
-		let data = null;
-		try {
-			data = JSON.parse(message.data);
-		} catch (e) {
-			console.error("Failed to parse message: " + message.data);
-		}
+	getResponseHandlers() {
+		const disableResponseReturn = /** @satisfies {import("https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/util/TypedMessenger.js").TypedMessengerRequestHandlerReturn} */ {
+			$respondOptions: {
+				respond: false,
+			},
+		};
 
-		if (!data) return;
-		const { op } = data;
-		if (!op) return;
-
-		if (op == "setIsStudioHost") {
-			const newIsHost = !!data.isHost;
-			if (newIsHost == this.#isStudioHost && this.#firstIsHostMessageReceived) return;
-			this.#isStudioHost = newIsHost;
-			this.#firstIsHostMessageReceived = true;
-			if (this.#isStudioHost) {
-				this.notifyNearbyHostConnectionsAdd();
-			} else {
-				this.sendNearbyHostConnectionsList();
-			}
-		} else if (op == "projectMetaData") {
-			const { projectMetaData } = data;
-			this.#projectMetaData = projectMetaData;
-			this.notifyNearbyHostConnectionsUpdateProjectMetaData();
-		} else if (op == "relayMessage") {
-			const { toUuid, data: relayData } = data;
-			if (!toUuid || !relayData) return;
-			const toConnection = this.#webSocketManager.getConnection(toUuid);
-			if (!toConnection) return;
-			toConnection.sendRelayData(this.#id, relayData);
-		}
-	}
-
-	onClose() {
-		if (this.#isStudioHost) {
-			this.notifyNearbyHostConnectionsRemove();
-		}
-	}
-
-	/**
-	 * @param {*} data
-	 */
-	send(data) {
-		this.#rawConnection.send(JSON.stringify(data));
-	}
-
-	getConnectionData() {
 		return {
-			id: this.#id,
-			clientType: this.clientType,
-			projectMetaData: this.#projectMetaData,
+			/**
+			 * Sets the client type of your client.
+			 * @param {import("https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/network/studioConnections/DiscoveryManager.js").ClientType} clientType True if the client has an available project.
+			 */
+			registerClient: (clientType) => {
+				if (this.#clientType) {
+					throw new Error("A client has already been registered. Clients can only be registered once. If you wish to use a different client type, you should create a new connection.");
+				}
+				this.#clientType = clientType;
+				this.#notifyNearbyHostConnectionsAdd();
+				this.#sendNearbyHostConnectionsList();
+			},
+			/**
+			 * @param {import("https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/network/studioConnections/DiscoveryManager.js").RemoteStudioMetadata?} projectMetadata
+			 */
+			setProjectMetadata: (projectMetadata) => {
+				this.#projectMetadata = projectMetadata;
+				this.#notifyNearbyHostConnectionsUpdateProjectMetadata();
+				return disableResponseReturn;
+			},
+			/**
+			 * Sends arbitrary data to another client that is currently connected.
+			 * @param {import("https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/util/util.js").UuidString} otherClientUuid
+			 * @param {import("https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/network/studioConnections/discoveryMethods/WebRtcDiscoveryMethod.js").ExternalDiscoveryRelayData} data
+			 */
+			relayMessage: (otherClientUuid, data) => {
+				if (otherClientUuid && data) {
+					const toConnection = this.#webSocketManager.getConnection(otherClientUuid);
+					if (toConnection) {
+						toConnection.#messenger.send.relayMessage(this.#uuid, data);
+					}
+				}
+				return disableResponseReturn;
+			},
 		};
 	}
 
-	sendNearbyHostConnectionsList() {
+	onClose() {
+		if (this.#clientType) {
+			this.#notifyNearbyHostConnectionsRemove();
+		}
+	}
+
+	/** @returns {import("https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/network/studioConnections/DiscoveryManager.js").AvailableStudioData?} */
+	#getConnectionData() {
+		if (!this.#clientType) return null;
+		return {
+			id: this.#uuid,
+			clientType: this.#clientType,
+			projectMetadata: this.#projectMetadata,
+		};
+	}
+
+	#sendNearbyHostConnectionsList() {
+		/** @type {import("https://raw.githubusercontent.com/rendajs/Renda/015df2634a012df1fdd144f059dcdcb17d3aa51e/src/network/studioConnections/DiscoveryManager.js").AvailableStudioData[]} */
 		const connectionsData = [];
 		for (const connection of this.#webSocketManager.getConnectionsByRemoteAddress(this.#remoteAddress)) {
-			if (!connection.#isStudioHost) continue;
-			connectionsData.push(connection.getConnectionData());
+			if (connection == this) continue;
+			const connectionData = connection.#getConnectionData();
+			if (!connectionData) continue;
+			connectionsData.push(connectionData);
 		}
-		this.send({
-			op: "nearbyHostConnectionsList",
-			connections: connectionsData,
-		});
+		this.#messenger.send.setAvailableConnections(connectionsData);
 	}
 
-	notifyNearbyHostConnectionsAdd() {
+	#notifyNearbyHostConnectionsAdd() {
+		const connectionData = this.#getConnectionData();
+		if (!connectionData) return;
 		for (const connection of this.#webSocketManager.getConnectionsByRemoteAddress(this.#remoteAddress)) {
-			if (connection.#isStudioHost) continue;
-
-			connection.sendNearbyHostConnectionAdded(this);
+			if (connection == this) continue;
+			connection.#messenger.send.addAvailableConnection(connectionData);
 		}
 	}
 
-	notifyNearbyHostConnectionsRemove() {
+	#notifyNearbyHostConnectionsRemove() {
 		for (const connection of this.#webSocketManager.getConnectionsByRemoteAddress(this.#remoteAddress)) {
-			if (connection.#isStudioHost) continue;
+			if (connection == this) continue;
 
-			connection.sendNearbyHostConnectionRemoved(this);
+			connection.#messenger.send.removeAvailableConnection(this.#uuid);
 		}
 	}
 
-	notifyNearbyHostConnectionsUpdateProjectMetaData() {
+	#notifyNearbyHostConnectionsUpdateProjectMetadata() {
 		for (const connection of this.#webSocketManager.getConnectionsByRemoteAddress(this.#remoteAddress)) {
-			if (connection.#isStudioHost) continue;
+			if (connection.#clientType) continue;
 
-			connection.sendNearbyHostConnectionUpdateProjectMetaData(this);
+			connection.#messenger.send.setConnectionProjectMetadata(this.#uuid, this.#projectMetadata);
 		}
-	}
-
-	/**
-	 * @param {WebSocketConnection} connection
-	 */
-	sendNearbyHostConnectionAdded(connection) {
-		this.send({
-			op: "nearbyHostConnectionAdded",
-			connection: connection.getConnectionData(),
-		});
-	}
-
-	/**
-	 * @param {WebSocketConnection} connection
-	 */
-	sendNearbyHostConnectionRemoved(connection) {
-		this.send({
-			op: "nearbyHostConnectionRemoved",
-			id: connection.#id,
-		});
-	}
-
-	/**
-	 * @param {WebSocketConnection} connection
-	 */
-	sendNearbyHostConnectionUpdateProjectMetaData(connection) {
-		this.send({
-			op: "nearbyHostConnectionUpdateProjectMetaData",
-			id: connection.#id,
-			projectMetaData: connection.#projectMetaData,
-		});
-	}
-
-	/**
-	 * @param {string} fromUuid
-	 * @param {*} data
-	 */
-	sendRelayData(fromUuid, data) {
-		this.send({
-			op: "relayMessage",
-			fromUuid,
-			data,
-		});
 	}
 }
